404fdea2ec8caf79d75277b063dbe3b9c009e5ce
Steven Schuler <stevenschuler5983@yahoo.ca>
2023-11-05 15:46:49 -0800

    Added ideas for future experiments. began baseline experiment to be used for comparison as well.

--- Super_AI_Bros.py
+++ Super_AI_Bros.py
@@ -12,51 +12,65 @@ env = JoypadSpace(env, SIMPLE_MOVEMENT)
 # Create a flag - restart or not
 done = True
 
-for step in range(100000):
-# Start the game to begin with
-    if done:
-        # Start the game
-        env.reset()
-    # Do actions
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    # Show the game on the screen
-    done = terminated or truncated
-    env.render()
-# Close the game
-env.close()
+# for step in range(100000):
+# # Start the game to begin with
+#     if done:
+#         # Start the game
+#         env.reset()
+#     # Do actions
+#     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+#     # Show the game on the screen
+#     done = terminated or truncated
+#     env.render()
+# # Close the game
+# env.close()
+
+
 
 #2.PREPROCESS ENVIRONMENT
 # Install pytorch
 # Import Frame Stacker Wrapper and GrayScaling Wrapper
 from gym.wrappers import GrayScaleObservation
-# Import Vectorization Wrappers
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-# Import Matplotlib to show the impact of frame stacking
+# # Import Vectorization Wrappers
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv, SubprocVecEnv, VecNormalize
+# # Import Matplotlib to show the impact of frame stacking
 from matplotlib import pyplot as plt
-# 1. Create the base environment
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-# 2. Simplify the controls 
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
-# 3. Grayscale
+# # 1. Create the base environment
+env = gym_super_mario_bros.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
+# # 2. Simplify the controls 
+env = JoypadSpace(env, SIMPLE_MOVEMENT) # EXPERIMENT: Change this to COMPLEX_MOVEMENT
+# # 3. Grayscale
 env = GrayScaleObservation(env, keep_dim=True)
-# 4. Wrap inside the Dummy Environment
-env = DummyVecEnv([lambda: env])
-# 5. Stack the frames
-env = VecFrameStack(env, 4, channels_order='last')
-state = env.reset()
-state, reward, done, info = env.step([5])
-plt.figure(figsize=(20,16))
-for idx in range(state.shape[3]):
-    plt.subplot(1,4,idx+1)
-    plt.imshow(state[0][:,:,idx])
-plt.show()
+# # 4. Wrap inside the Dummy Environment
 
-#3.TRAIN THE MODEL
-# Import os for file path management
+env = DummyVecEnv([lambda: env])
+#env = SubprocVecEnv([lambda: env], start_method="spawn") # EXPERIMENT: Try to change how we run the simulations
+
+
+# # 5. Stack the frames
+env = VecFrameStack(env, 4, channels_order='last')
+
+# # 6. Normalize the observation/rewards/both
+# env = VecNormalize(env, norm_obs=True) # EXPERIMENT: See if this normalization (on observation, or on rewards with norm_rewards=True) changes performance of Agent
+
+# # 7. Draw the initial preprocessed screen 
+# state = env.reset()
+# state, reward, done, info = env.step([5])
+plt.figure(figsize=(20,16))
+
+# for idx in range(state.shape[3]):
+#     plt.subplot(1,4,idx+1)
+#     plt.imshow(state[0][:,:,idx])
+# plt.show()
+
+
+
+# #3.TRAIN THE MODEL
+# # Import os for file path management
 import os 
-# Import PPO for algos
+# # Import PPO for algos
 from stable_baselines3 import PPO
-# Import Base Callback for saving models
+# # Import Base Callback for saving models
 from stable_baselines3.common.callbacks import BaseCallback
 class TrainAndLoggingCallback(BaseCallback):
 
@@ -78,25 +92,28 @@ class TrainAndLoggingCallback(BaseCallback):
 CHECKPOINT_DIR = './train/'
 LOG_DIR = './logs/'
 # Setup model saving callback
-callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
 # This is the AI model started
-model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, 
-            n_steps=512) 
+model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, # EXPERIMENT: Adjust this learning rate
+            n_steps=512)  # EXPERIMENT: Try using 'MlpPolicy' or "MultiInputPolicy" instead
 # Train the AI model, this is where the AI model starts to learn
-model.learn(total_timesteps=1000000, callback=callback)
-model.save('thisisatestmodel')
+model.learn(total_timesteps=10000, callback=callback) # EXPERIMENT: train for longer periods of time to see how it improves over that time span
+model.save('test_baseline')
 
 
 #4.TEST THE MODEL
 
 # Load model
-model = PPO.load('./train/best_model_1000000')
-state = env.reset()
-# Start the game 
-state = env.reset()
-# Loop through the game
-while True: 
-    
-    action, _ = model.predict(state)
-    state, reward, done, info = env.step(action)
-    env.render()
+# model = PPO.load('./test_name')
+# state = env.reset()
+# # Start the game 
+# # state = env.reset()
+# # Loop through the game
+
+
+# while True: 
+
+#     action, _ = model.predict(state)
+#     state, reward, done, info = env.step(action)   
+        
+#     env.render()
