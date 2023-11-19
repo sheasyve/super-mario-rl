0d8ee7ba24046c6047f35ce627822d076409a9b2
Vickyyu2000 <churongyu0729@gmail.com>
2023-11-19 15:08:28 -0800

    adding learning rate scheduler

--- src/Super_AI_Bros.py
+++ src/Super_AI_Bros.py
@@ -3,32 +3,33 @@
 import gym_super_mario_bros
 
 # Import the SIMPLIFIED controls
-from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, RIGHT_ONLY 
 
 # Import the Joypad wrapper
 from nes_py.wrappers import JoypadSpace
 
-# Setup game
+#https://github.com/yumouwei/super-mario-bros-reinforcement-learning/blob/main/gym_utils.py
+from gym_utils import SMBRamWrapper
+from stable_baselines3.common.monitor import Monitor
 
+import os
+
+# # Import PPO for algos
+from stable_baselines3 import PPO
+
+# # Import Base Callback for saving models
+from stable_baselines3.common.callbacks import BaseCallback
+
+# 2.PREPROCESS ENVIRONMENT
+# Setup game
+# env = gym_super_mario_bros.make(
+#     'SuperMarioBros-1-1-v0'
+# )
 env = gym_super_mario_bros.make(
     "SuperMarioBros-v0", apply_api_compatibility=True, render_mode="rgb_array"
 )
-env = JoypadSpace(env, COMPLEX_MOVEMENT)
-# Create a flag - restart or not
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
 done = True
-
-# for step in range(100000):
-# # Start the game to begin with
-#     if done:
-#         # Start the game
-#         env.reset()
-#     # Do actions
-#     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-#     # Show the game on the screen
-#     done = terminated or truncated
-#     env.render()
-# # Close the game
-# env.close()
 
 
 # 2.PREPROCESS ENVIRONMENT
@@ -52,7 +53,7 @@ env = gym_super_mario_bros.make(
     "SuperMarioBros-v0", apply_api_compatibility=True, render_mode="rgb_array"
 )
 # # 2. Simplify the controls
-env = JoypadSpace(env, COMPLEX_MOVEMENT)  # EXPERIMENT: Change this to COMPLEX_MOVEMENT
+env = JoypadSpace(env, SIMPLE_MOVEMENT)  # EXPERIMENT: Change this to COMPLEX_MOVEMENT
 # # 3. Grayscale
 env = GrayScaleObservation(env, keep_dim=True)
 # # 4. Wrap inside the Dummy Environment
@@ -72,27 +73,11 @@ env = VecNormalize(env, training=True, norm_obs=True, norm_reward=True, clip_obs
 #Observation only 
 #env = VecNormalize(env, training=True, norm_obs=True, norm_reward=False, clip_obs=10.0, clip_reward=10.0, gamma=0.99, epsilon=1e-08) # EXPERIMENT: See if this normalization (on observation, or on rewards with norm_rewards=True) changes performance of Agent
 
-# # 7. Draw the initial preprocessed screen
-state = env.reset()
-state, reward, done, info = env.step([5])
-plt.figure(figsize=(20, 16))
-
-for idx in range(state.shape[3]):
-    plt.subplot(1,4,idx+1)
-    plt.imshow(state[0][:,:,idx])
-plt.show()
-
 
 # #3.TRAIN THE MODEL
 # # Import os for file path management
-import os
 
-# # Import PPO for algos
-from stable_baselines3 import PPO
-
-# # Import Base Callback for saving models
-from stable_baselines3.common.callbacks import BaseCallback
-
+# # Callbacks
 
 class TrainAndLoggingCallback(BaseCallback):
     def __init__(self, check_freq, save_path, verbose=1):
@@ -113,38 +98,56 @@ class TrainAndLoggingCallback(BaseCallback):
 
         return True
 
+# Linear learning rate schedule
+# https://stable-baselines3.readthedocs.io/en/master/guide/examples.html#learning-rate-schedule
+from typing import Callable
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
+
 CHECKPOINT_DIR = "./train/"
 LOG_DIR = "./logs/"
 # Setup model saving callback
-callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
+callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+
 # This is the AI model started
 model = PPO(
     "MlpPolicy",
     env,
     verbose=1,
     tensorboard_log=LOG_DIR,
-    learning_rate=0.000001,  # EXPERIMENT: Adjust this learning rate
-    n_steps=512,
-)  # EXPERIMENT: Try using 'MlpPolicy' or "MultiInputPolicy" instead
+    learning_rate=linear_schedule(3e-4),
+    n_steps=2056,
+) 
+
 # Train the AI model, this is where the AI model starts to learn
+import time
+t_start = time.time()
+
 model.learn(
-    total_timesteps=20000, callback=callback
+    total_timesteps=100000, callback=callback
 )  # EXPERIMENT: train for longer periods of time to see how it improves over that time span
-model.save("archive/temp.model")
 
-# 4.TEST THE MODEL
+t_elapsed = time.time() - t_start
+print('Wall time: {} s'.format(round(t_elapsed, 2)))
 
-# Load model
-# model = PPO.load('./test_name')
-# state = env.reset()
-# # Start the game
-# # state = env.reset()
-# # Loop through the game
+# Save model
 
-
-# while True:
-
-#     action, _ = model.predict(state)
-#     state, reward, done, info = env.step(action)
-
-#     env.render()
+model_path = "./model_zip/"
+model.save(model_path)
