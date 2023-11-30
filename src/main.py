86efb21a07c5540f9d1a8ac026ccb3e125a77197
sheasyve <113799482+sheasyve@users.noreply.github.com>
2023-11-29 19:27:29 -0800

    Update main.py
    Added citation

--- src/main.py
+++ src/main.py
@@ -184,5 +184,13 @@ def main():
     
 if __name__ == "__main__":
     main()
-
+'''
+@misc{gym-super-mario-bros,
+  author = {Christian Kauten},
+  howpublished = {GitHub},
+  title = {{S}uper {M}ario {B}ros for {O}pen{AI} {G}ym},
+  URL = {https://github.com/Kautenja/gym-super-mario-bros},
+  year = {2018},
+}
+'''
     
